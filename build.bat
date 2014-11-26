e94eb2f82699526eb813b0e471af43c3cc781841
Guilherme <guifes@gmail.com>
2014-11-26 02:56:32 -0200

    Day 002
    Creating a Windows Window and painting it's buffer;

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 
 mkdir ..\..\build
 pushd ..\..\build
-cl -Zi ..\lupos\code\win32_lupos.cpp user32.lib
+cl -Zi ..\lupos\code\win32_lupos.cpp user32.lib Gdi32.lib
 popd
---
+++ debug.bat
@@ -0,0 +1,3 @@
+@echo off
+
+devenv "..\build\win32_lupos.exe"
